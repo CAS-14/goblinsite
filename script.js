45e5adf537e773a0d8b153ca4b979fcd6442c7a5
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-08 19:49:22 -0400

    loop back around

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ var imageIndex = 0;
 
 function cycleBackground() {
     imageIndex += 1;
+
+    if (imageIndex >= images.length()) {
+        imageIndex = 0;
+    }
+
     document.body.style.backgroundImage = "url('assets/"+images[imageIndex]+"')"
 }
