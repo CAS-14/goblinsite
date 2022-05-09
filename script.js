5911f8330c31f8103a1bf8330fbbe3815f61d067
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-09 12:12:20 -0400

    fix

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 function cycleBackground() {
     var imageIndex = localStorage.getItem("imageIndex");
     if (imageIndex == null) { 
-        imageIndex = 1;
-        console.log("image index initialized to 1")
+        imageIndex = 0;
+        console.log("image index initialized to 0")
     }
 
-    console.log("image index before increment : "+imageIndex);
+    console.log("image index before increment: "+imageIndex);
 
     imageIndex ++;
     if (imageIndex > 3) imageIndex = 1;
 
-    loadBackground(imageIndex);
-    console.log("image index after increment  : "+imageIndex);
-}
-
-function loadBackground(imageIndex) {
-    document.body.style.backgroundImage = "url('assets/background/bg"+imageIndex+".png')";
+    document.body.style.backgroundImage = "url('assets/background/bg" + imageIndex + ".png')";
     localStorage.setItem("imageIndex", imageIndex);
+    console.log("image index after load: "+imageIndex);
 }
