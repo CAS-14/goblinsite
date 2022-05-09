e95f27b144c8965d9b0c9e73c7aed0e4ab26cefb
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-09 12:07:51 -0400

    clean up the code

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 function cycleBackground() {
     var imageIndex = localStorage.getItem("imageIndex");
-    if (imageIndex == null) {
-        imageIndex = 1;
-    }
+    if (imageIndex == null) imageIndex = 1;
 
-    console.log("image index before increment: "+imageIndex);
+    console.log("image index before increment : "+imageIndex);
 
-    imageIndex += 1;
-    if (imageIndex > 3) {
-        imageIndex = 1;
-    }
+    imageIndex ++;
+    if (imageIndex > 3) imageIndex = 1;
 
     loadBackground(imageIndex);
-    console.log("image index after incrementing: "+imageIndex);
+    console.log("image index after increment  : "+imageIndex);
 }
 
 function loadBackground(imageIndex) {
