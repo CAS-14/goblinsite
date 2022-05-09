a41e52cd31af35b7824af8f6ff508a066225e3f7
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-08 20:41:04 -0400

    ghjgjhgjhgjhgjh

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-var imageIndex = localStorage.getItem("imageIndex");
-if (imageIndex == null) {
-    imageIndex = 1;
-}
-
-loadBackground(imageIndex)
-
 function cycleBackground() {
+    var imageIndex = localStorage.getItem("imageIndex");
+    if (imageIndex == null) {
+        imageIndex = 1;
+    }
+
+    console.log("image index before increment: "+imageIndex);
+
     imageIndex += 1;
     if (imageIndex > 3) {
         imageIndex = 1;
     }
 
     loadBackground(imageIndex);
+    console.log("image index after incrementing: "+imageIndex);
 }
 
 function loadBackground(imageIndex) {
