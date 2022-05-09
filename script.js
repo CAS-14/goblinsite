7a31063eeb1f48439639294905d86c87929657a2
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-09 12:24:05 -0400

    fix script errs

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-function cycleBackground() {
+function cycleBackground(doIncrement) {
     var imageIndex = localStorage.getItem("imageIndex");
-    if (imageIndex == null) { 
-        imageIndex = 0;
-        console.log("image index initialized to 0")
-    }
+    if (imageIndex == null) imageIndex = 1;
 
-    console.log("image index before increment: "+imageIndex);
-
-    imageIndex ++;
+    if (doIncrement) imageIndex ++;
     if (imageIndex > 3) imageIndex = 1;
 
     rs = document.querySelector(":root").style;
     rs.setProperty("--background", "url('assets/background/bg" + imageIndex + ".png')");
-
     localStorage.setItem("imageIndex", imageIndex);
-    console.log("image index after load: "+imageIndex);
 }
